a0bc8be5c506bb2ba754210e1f1d00f783ad29a7
Arivonil <arivonilsampaio@users.noreply.github.com>
2025-01-24 08:48:24 -0300

    23-01-25

--- index.php
+++ index.php
@@ -1,36 +1,62 @@
 <?php
 
-$dsn = 'mysql:host=localhost;dbname=php_com_pdo';
-$usuario = 'root';
-$senha = '';
+if (!empty($_POST['usuario']) && !empty($_POST['senha'])) {
 
-try {
-  $conexao = new PDO($dsn, $usuario, $senha);
+  $dsn = 'mysql:host=localhost;dbname=php_com_pdo';
+  $usuario = 'root';
+  $senha = '';
 
-  $query = '
-   select * from tb_usuarios
-   ';
-   
-  //  $stmt = $conexao->query($query);
-   
-   foreach($conexao->query($query) as $key => $value) {
-     print_r($value['nome']);
-     echo '<hr>';
-   }
-   
-  //  $lista_usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
-  //  echo '<pre>';
-  //  print_r($lista_usuarios);
-  //  echo '</pre>';
-  
-  // foreach($lista_usuarios as $key => $value) {
-  //   print_r($value['nome']);
-  //   echo '<hr>';
-  // }
-  
-   
-  
-} catch(PDOException $e) {
-  echo 'Erro' . $e->getCode() . ' Mensagem ' . $e->getMessage();
-  //registrar erro
+  try {
+    $conexao = new PDO($dsn, $usuario, $senha);
+    
+    $query = "select * from tb_usuarios where";
+    $query .= " email = :usuario";
+    $query .= " AND senha = :senha";
+    
+    $stmt =  $conexao->prepare($query);
+    
+    $stmt->bindValue(':usuario', $_POST['usuario']);
+    $stmt->bindValue(':senha', $_POST['senha']);
+    
+    $stmt->execute();
+    
+    $usuario = $stmt->fetch();
+    
+    echo '<pre>';
+    print_r($usuario);
+    echo '</pre>';
+    
+    
+    
+    
+  } catch (PDOException $e) {
+    echo 'Erro' . $e->getCode() . ' Mensagem ' . $e->getMessage();
+    //registrar erro
+  }
 }
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login</title>
+</head>
+
+<body>
+  <form method="post" action="index.php">
+    <input type="text" placeholder="usuário" name="usuario">
+    <br>
+    <input type="password" placeholder="senha" name="senha">
+    <br>
+    <br>
+    <button type="submit">Entrar</button>
+
+  </form>
+</body>
+
+</html>
